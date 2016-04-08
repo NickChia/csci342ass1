981c0b212119a564ecbf6a2bc90b23bb41213dfd
Nick <nick@nickdemac.local>
2016-04-08 17:11:06 +1000

    commit

---
+++ 342A1/TileView.swift
@@ -0,0 +1,16 @@
+//
+//  TileView.swift
+//  342A1
+//
+//  Created by Nick on 16/4/8.
+//  Copyright © 2016年 Nick. All rights reserved.
+//
+
+import UIKit
+
+class TileView: UIView {
+    
+    required init(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)!
+    }
+}
