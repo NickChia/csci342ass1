5c1ae922b7c6a268e6c48ed9f23861846d20f36a
Nick <nick@nickdemac.local>
2016-04-08 19:42:56 +1000

    commit

--- 342A1/ViewController.swift
+++ 342A1/ViewController.swift
@@ -8,22 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, TileViewDelegator{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        var tileViews : [TileView] = []
+        let numOfTiles: Int = 12
+        
         let images:[UIImage] = [UIImage(named: "baldhill")!, UIImage(named: "cathedral")!, UIImage(named: "lake")!]
-        var instance : GameModel = GameModel(totalTileNumber: 12, images: images)
+        var instance : GameModel = GameModel(totalTileNumber: numOfTiles, images: images)
         print(instance)
+        
+        for i in 1...numOfTiles {
+            var tileView = view.viewWithTag(i) as! TileView
+            tileViews.append(tileView)
+            tileView.delegator = self
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func didSelectTile(tileView: TileView){
+    
+    }
 }
 
